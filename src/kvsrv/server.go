40a091673bc6c2a315084a1f31825cd6cb5f9833
moYu <jzf221419168@163.com>
2025-05-30 14:58:04 +0800

    complete the basic RPC of KvServer, it is necessary to improve the situation of message loss

--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -14,29 +14,41 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type KVServer struct {
-	mu sync.Mutex
-
+	mu  sync.Mutex
+	Map map[string]string
 	// Your definitions here.
 }
 
-
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	val, ok := kv.Map[args.Key]
+	if ok {
+		reply.Value = val
+	}
 }
 
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	kv.Map[args.Key] = args.Value
 }
 
 func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	oldValue := kv.Map[args.Key]
+	kv.Map[args.Key] = oldValue + args.Value
+	reply.Value = oldValue
 }
 
 func StartKVServer() *KVServer {
 	kv := new(KVServer)
-
+	kv.Map = make(map[string]string)
 	// You may need initialization code here.
 
 	return kv
